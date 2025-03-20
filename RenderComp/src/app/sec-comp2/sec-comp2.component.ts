8c3a41e1919975a1b1d0d22a8d53f25f302f2912
SuhaniBhosale375 <suhanibhosale375@gmail.com>
2025-03-20 22:00:29 +0530

    CostomComponent

---
+++ RenderComp/src/app/sec-comp2/sec-comp2.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sec-comp2',
+  imports: [],
+  templateUrl: './sec-comp2.component.html',
+  styleUrl: './sec-comp2.component.css'
+})
+export class SecComp2Component {
+
+}
