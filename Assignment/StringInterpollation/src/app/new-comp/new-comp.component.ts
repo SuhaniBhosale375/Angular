bd907ada22d2ece2263cd8e7a68ab5c5ed546d81
SuhaniBhosale375 <suhanibhosale375@gmail.com>
2025-03-29 09:53:56 +0530

    Assignment 6 Q 4

---
+++ Assignment/StringInterpollation/src/app/new-comp/new-comp.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-comp',
+  imports: [],
+  templateUrl: './new-comp.component.html',
+  styleUrl: './new-comp.component.css'
+})
+export class NewCompComponent {
+  public name = "Suhani";
+  public str = "Hello";
+}
