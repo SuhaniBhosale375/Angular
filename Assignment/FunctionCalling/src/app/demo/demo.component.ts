80d7c55b0db495f7ee3c76b15e5050ad6fbd6625
SuhaniBhosale375 <suhanibhosale375@gmail.com>
2025-04-01 11:37:26 +0530

    Assignent no 7 Q 2

---
+++ Assignment/FunctionCalling/src/app/demo/demo.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-demo',
+  imports: [],
+  templateUrl: './demo.component.html',
+  styleUrl: './demo.component.css'
+})
+export class DemoComponent {
+    public fun()
+    {
+        return "Hello from fun fuction"
+    }
+}
