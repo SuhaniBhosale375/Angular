9e21bef2eb4bda38b637afd6c6a90e64e112a563
SuhaniBhosale375 <suhanibhosale375@gmail.com>
2025-04-01 11:34:54 +0530

    Assignent no 7 Q 1

---
+++ Assignment/CostomComponent/src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { FirstComponent } from './first/first.component';
+import { SecondComponent } from './second/second.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [
+    FirstComponent,
+    SecondComponent
+
+  ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'Demo3';
+}
